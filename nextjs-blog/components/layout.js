521aeaf149b0cb8a0649970dd734f9c504ef4d7e
cdh77 <orbitjazzy61@gmail.com>
2020-09-02 04:14:23 +0000

    Change title name

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'cdh77'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js Blog'
 
 export default function Layout({ children, home }) {
   return (
